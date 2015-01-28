4938af2026375d78a05f175dabb42fadf5b7daaf
Miguel Quintero <migmaqer@gmail.com>
2015-01-28 00:28:25 -0500

    fix jscs lint warning
    Change-Id: I130f76aa3b4e600f6103d590e6bfcedcbe0984e0

--- projects-broker/lib/project-utils.js
+++ projects-broker/lib/project-utils.js
@@ -5,7 +5,8 @@ var exec = require('child_process').exec;
 module.exports = {
 
   /*
-  cmd: Command to run, with space-separated arguments, example: /bin/bash /var/lib/forj/newproject.sh foo
+  cmd: Command to run, with space-separated arguments
+  example: /bin/bash /var/lib/forj/newproject.sh foo
   callback(error, stdout, stderr):
     On success: error will be null.
     On error: error.code will be the exit code
